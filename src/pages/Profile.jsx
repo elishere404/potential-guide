2d7343f303f507beb65409aa3f663c8bf18f7f9d
elis <141450459+elishere404@users.noreply.github.com>
2024-12-04 18:51:44 +0400

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,8 +10,8 @@ export default function Profile() {
      <h1 class="text-lg text-gray-700"> Andrew </h1>
      <p class="text-xs text-gray-400 mt-4"> My name is Andrew, i do lots of things - play games, programming, etc. I really enjoy it, reach out to me on Github or Email </p>
      <div className="flex flex-row justify-center items-center gap-4">
-      <a href="https://github.com/elishere404"><img src="https://ibb.co/S0Nj3L1" alt="github" /></a>
-      <a href="mailto:hilariousaction@gmail.com"><img src="https://ibb.co/Vqf7FvW" alt="gmail" /></a>
+      <a href="https://github.com/elishere404"><img src="https://i.ibb.co/TRcfgCn/github.png" alt="github" /></a>
+      <a href="mailto:hilariousaction@gmail.com"><img src="https://i.ibb.co/pLYtH3v/email.png" alt="gmail" /></a>
      </div>
    </div>
  </div>
